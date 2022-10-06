5e746b729bf7f2dd32b193c60cb4db2c9b9b1cf5
Bradley Donn Kukuk <brad@bdksoftware.com>
2022-10-06 14:20:03 -0600

    Changing cheveron to menu icon

--- components/DropDown.jsx
+++ components/DropDown.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
+import { Feather } from "@expo/vector-icons";
 
 const DropDown = ({ setCategory, category, categories }) => {
   return (
@@ -8,6 +9,7 @@ const DropDown = ({ setCategory, category, categories }) => {
       <AutocompleteDropdown
         clearOnFocus={false}
         closeOnBlur={false}
+        ChevronIconComponent={<Feather name="menu" size={16} color="grey" />}
         initialValue={{ id: "2" }} // or just '2'
         onSelectItem={(item) => {
           item && setCategory(item.title);
